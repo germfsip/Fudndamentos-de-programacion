bb22f37cffa8c703044479a17a5c74848533abc7
Gerardo Martinez <84603889+germfsip@users.noreply.github.com>
2022-08-19 14:35:38 +0000

    primera version

---
+++ main.c
@@ -0,0 +1,27 @@
+//Codigo para calcular tiempo de certificacion
+//Gerardo Mtz
+//19 de agosto 2022
+//V 1
+
+//Bibliotecas
+#include <stdio.h>
+
+
+int main(void) {
+  //Variables
+  int Tcert = 60;
+  int Tdedicado;
+  float Trequerido;
+  
+  //Ingresar los valores
+  printf("Ingresa el tiempo a estudiar en minutos\n");
+  scanf("%d", &Tdedicado);
+
+  //Operacion 
+  Trequerido = Tcert*60/Tdedicado;
+
+  //Imprimir resultados
+  printf("Se requieren %f dias para obtener el certificado", Trequerido);
+  
+  return 0;
+}
